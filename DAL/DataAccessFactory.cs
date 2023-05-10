80ea2b4d5a1b2299fb759d2f39a8ef7ee11fe651
JayedChowdhuryRabby <19-40893-2@student.aiub.edu>
2023-05-10 15:07:17 +0600

    init

---
+++ DAL/DataAccessFactory.cs
@@ -0,0 +1,77 @@
+﻿using DAL.Interface;
+using DAL.Models;
+using DAL.Models.Login;
+using DAL.Models.SignUp;
+using DAL.Repos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DataAccessFactory
+    {
+        public static IAuth<bool> AuthData()
+        {
+            return new UserRepo();
+        }
+        public static IRepo<Token, string, Token> TokenData()
+        {
+            return new TokenRepo();
+        }
+        public static IRepo<User, int, bool> UserData()
+        {
+            return new UserRepo();
+        }
+        public static IRepo<Category, int, bool> CategoryData()
+        {
+            return new CategoryRepo();
+        }
+        public static IRepo<Product, int, bool> ProductData()
+        {
+            return new ProductRepo();
+        }
+        public static IRepo<Seller, int, bool> SellerData()
+        {
+            return new SellerRepo();
+        }
+        public static IRepo<Admin, int, bool> AdminData()
+        {
+            return new AdminRepo();
+        }
+        public static IRepo<Cart, int, bool> CartData()
+        {
+            return new CartRepo();
+        }
+        public static IRepo<Pay, int, bool> PayData()
+        {
+            return new PayRepo();
+        }
+        public static IRepo<Customer, int, bool> CustomerData()
+        {
+            return new CustomerRepo();
+        }
+        public static IRepo<Order, int, bool> OrderData()
+        {
+            return new OrderRepo();
+        }
+        public static IRepo<OrderDetail, int, bool> OrderDetailData()
+        {
+            return new OrderDetailRepo();
+        }
+        public static IRepo<DeliveryMan, int, bool> DeliveryManData()
+        {
+            return new DeliveryManRepo();
+        }
+        public static IRepo<Promotion, int, bool> PromotionData()
+        {
+            return new PromotionRepo();
+        }
+        public static IRepo<Search, int, bool> SearchData()
+        {
+            return new SearchRepo();
+        }
+    }
+}
---
+++ DAL/SwiftSaleContext.cs
@@ -0,0 +1,30 @@
+﻿using DAL.Models;
+using DAL.Models.Login;
+using DAL.Models.SignUp;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    internal class SwiftSaleContext:DbContext
+    {
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Seller> Sellers { get; set; }   
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get;set; }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Pay> Pays { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<DeliveryMan> DeliveryMans { get; set; }
+        public DbSet<Search> Searchs { get; set; }
+    }
+}
